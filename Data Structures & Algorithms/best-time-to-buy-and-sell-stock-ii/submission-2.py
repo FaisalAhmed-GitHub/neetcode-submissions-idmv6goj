271392596d6e91c8f479624f2e0c16cbb38e06f3
FaisalAhmed-GitHub <95086274+FaisalAhmed-GitHub@users.noreply.github.com>
2026-05-21 17:39:01 +0530

    Add: best-time-to-buy-and-sell-stock-ii - submission-2

---
+++ Data Structures & Algorithms/best-time-to-buy-and-sell-stock-ii/submission-2.py
@@ -0,0 +1,15 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        
+        # self with a mistake at line 11
+        l , r  = 0 , 1
+        MaxP = 0
+        while r < len(prices):
+            if prices[l] < prices[r]:
+                Profit = prices[r] - prices[l]
+                MaxP = MaxP + Profit
+                l = r
+            else: 
+                l = r
+            r += 1
+        return MaxP
